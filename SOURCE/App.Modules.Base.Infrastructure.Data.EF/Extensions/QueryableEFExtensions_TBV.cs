5a11c261bddba3ac702e780adf57bfa6021ff75c
Sky S <skys.gh@ministryof.tech>
2026-01-25 17:36:52 +1300

    feat: Add EF Core extension methods
    ModelBuilderExtensions + QueryableEFExtensions + assembly scanner

---
+++ SOURCE/App.Modules.Base.Infrastructure.Data.EF/Extensions/QueryableEFExtensions_TBV.cs
@@ -0,0 +1,60 @@
+﻿// Extensions are always put in root namespace
+// for maximum usability from elsewhere:
+
+namespace App
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// <para>
+    /// Class to allow for use of EF's Include statement
+    /// from other assemblies, without needing to take 
+    /// a direct reference to EF.
+    /// Ie. App.Base.Domain Services and any specific Repositories 
+    /// can use 'Include'. 
+    /// </para>
+    /// </summary>
+    public static class QueryableEFExtensions
+    {
+        ///// <summary>
+        ///// TODO: Improve documentation
+        ///// </summary>
+        ///// <param name="source"></param>
+        ///// <param name="path"></param>
+        ///// <returns></returns>
+        //public static IQueryable Include(this IQueryable source, string path)
+        //{
+        //    return EntityFrameworkQueryableExtensions.Include(source, path);
+        //}
+
+        /// <summary>
+        /// TODO: Improve documentation
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static IQueryable<T> Include<T>(this IQueryable<T> source, string path)
+            where T:class
+        {
+            return EntityFrameworkQueryableExtensions.Include<T>(source, path);
+        }
+
+        ///// <summary>
+        ///// TODO: Improve documentation
+        ///// </summary>
+        ///// <typeparam name="T"></typeparam>
+        ///// <typeparam name="TProperty"></typeparam>
+        ///// <param name="source"></param>
+        ///// <param name="path"></param>
+        ///// <returns></returns>
+        //public static IQueryable<T> Include<T, TProperty>(this IQueryable<T> source,
+        //    Expression<Func<T, TProperty>> path)
+        //{
+        //    return EntityFrameworkQueryableExtensions.Include<T>(source, path);
+        //}
+    }
+}
---
+++ SOURCE/App.Modules.Base.Infrastructure.Data.EF/Extensions/ScanAllModuleAssembliesForImplementationsOfIDbCommitPreCommitProcessingStrategy_TBV.cs
@@ -0,0 +1,42 @@
+﻿using App.Base.Infrastructure.Services;
+using App.Base.Infrastructure.Storage.Db.EF.DbContexts.Implementations.Base;
+using App.Base.Infrastructure.Storage.Db.EF.Interceptors;
+using Lamar.Scanning.Conventions;
+using Microsoft.EntityFrameworkCore;
+
+namespace App
+{
+    /// <summary>
+    /// Scanner to teach the IoC to 
+    /// find and register all implementions
+    /// of 
+    /// <see cref="IDbCommitPreCommitProcessingStrategy"/>
+    /// which is the contract for processors/handlers that 
+    /// are invoked by
+    /// <see cref="IDbContextPreCommitService"/>
+    /// when invoked in the 
+    /// <see cref="DbContext.SaveChanges()"/>
+    /// method 
+    /// of 
+    /// <see cref="ModuleDbContextBase"/>
+    /// </summary>
+    public static class IServiceScannerExtensions 
+    {
+        /// <summary>
+        /// Teach Lamar to 
+        /// scan all Assemblies for implementations of 
+        /// <see cref="IDbCommitPreCommitProcessingStrategy"/>
+        /// so that a base <c>DbCommit.SaveChanges</c>
+        /// can invoke them.
+        /// </summary>
+        /// <param name="assemblyScanner"></param>
+        public static void ScanForImplementationsOfIDbCommitPreCommitProcessingStrategies(this IAssemblyScanner assemblyScanner)
+        {
+            // Add all Pre-Commit Processors (these kick in just as you
+            // Commit a DbContext, and ensure specific fields are 
+            // automatically filled in)
+            assemblyScanner.AddAllTypesOf<IDbCommitPreCommitProcessingStrategy>().NameBy(x => x.Name.Replace("Widget", "")); ;
+        }
+
+    }
+}
